552198e8297bd55e5b05016a24c1584933e5eb1b
Manish Kumar <mk9390996@gmail.com>
2025-07-04 22:58:47 +0530

    Question3 Completed

--- assignment/question3.cpp
+++ assignment/question3.cpp
@@ -1,1 +1,33 @@
 // Q3: Given a matrix ‘A’ of dimension n x m and 2 coordinates (l1, r1) and (l2, r2). Return the sum of the rectangle from (l1,r1) to (l2, r2).
+#include<iostream>
+using namespace std;
+int main(){
+    int m, n;
+    cout << "Enter the number of rows: ";
+    cin >> m;
+    cout << "Enter the number of columns: ";
+    cin >> n;
+
+    int arr[m][n];
+    cout << "Enter the elements of the matrix:" << endl;
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    int l1, r1, l2, r2;
+    cout << "Enter coordinates (l1, r1) and (l2, r2): ";
+    cin >> l1 >> r1 >> l2 >> r2;
+
+    int sum = 0;
+    for(int i = l1; i <= l2; i++){
+        for(int j = r1; j <= r2; j++){
+            sum += arr[i][j];
+        }
+    }
+
+    cout << "The sum of the rectangle from (" << l1 << ", " << r1 << ") to (" << l2 << ", " << r2 << ") is: " << sum << endl;
+
+    return 0;
+}
